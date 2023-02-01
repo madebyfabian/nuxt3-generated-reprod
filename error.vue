2205cba2a9786139f5fac7112c8e80ab2b159051
madebyfabian <hello@madebyfabian.com>
2023-02-01 21:31:44 +0100

    wip: try out adding `error.vue`

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<p>Custom error template:</p>
+		<pre>{{ props }}</pre>
+	</div>
+</template>
+
+<script setup lang="ts">
+	const props = defineProps({
+		error: Object,
+	})
+</script>
+
+<style scoped></style>
