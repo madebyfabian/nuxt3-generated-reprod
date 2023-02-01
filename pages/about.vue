0a14775e575dd15a6165f93d988e21bf2eac4065
madebyfabian <hello@madebyfabian.com>
2023-02-01 21:17:25 +0100

    init

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		About Page
+		<br />
+		<NuxtLink to="/">Home</NuxtLink>
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+	<div>
+		Home Page
+		<br />
+		<NuxtLink to="/about">About</NuxtLink>
+		<br />
+		<NuxtLink to="/blahh">Will be 404</NuxtLink>
+	</div>
+</template>
