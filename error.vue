e7948a825669ff63e9f9c5b391dfe136548cbaaf
madebyfabian <hello@madebyfabian.com>
2023-02-04 11:35:28 +0100

    fix: adding a `layouts/default.vue` to demonstrate issue in ticket https://github.com/nuxt/nuxt/issues/18718

--- error.vue
+++ error.vue
@@ -1,8 +1,12 @@
 <template>
 	<div>
 		<p>This piece of text is inside the <code>error.vue</code></p>
-		<p>This error template template does not uses any layouts at all.</p>
+		<p>
+			As you can see, the <code>layouts/default.vue</code> is not used, the
+			header is missing!
+		</p>
 		<pre>these props are cool to get some info: {{ props }}</pre>
+		<NuxtLink to="/">Home</NuxtLink>
 	</div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		Home Page
+		<h1>Home Page</h1>
 		<br />
 		<NuxtLink to="/about">About</NuxtLink>
 		<br />
