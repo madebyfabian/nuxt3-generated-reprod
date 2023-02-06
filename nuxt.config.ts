85f126fbf361c2c5767a0df5bfc92ec2eb8c5a2c
madebyfabian <hello@madebyfabian.com>
2023-02-06 07:28:23 +0100

    feat: implement `nuxt-seo-kit` minimal example

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	extends: ['nuxt-seo-kit'],
 
+	runtimeConfig: {
+		public: {
+			siteUrl: 'https://example.com',
+			siteName: 'Site name from nuxt.config',
+			language: 'en', // prefer more explicit language codes like `en-AU` over `en`
+		},
+	},
+
+	app: {
+		head: {
+			titleTemplate: '%pageTitle %titleSeparator %siteName',
+		},
+	},
 })
