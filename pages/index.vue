5ac87e54d7b8e1c91e6195b5da042434729c9647
madebyfabian <hello@madebyfabian.com>
2023-02-06 06:03:34 +0100

    wip: add test image

--- pages/index.vue
+++ pages/index.vue
@@ -5,5 +5,10 @@
 		<NuxtLink to="/about">About</NuxtLink>
 		<br />
 		<NuxtLink to="/blahh">Will be 404</NuxtLink>
+		<br />
+		<hr />
+		<br />
+		<h1>Test Image</h1>
+		<img src="/img/icon.svg" height="200px" alt="icon" />
 	</div>
 </template>
